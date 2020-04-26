f9daaec11bf5c94ef3fc375fc3a4c7f87cba6911
Pritesh Tupe <priteshtupe@gmail.com>
2020-04-26 19:00:05 +0530

    Added a working crawler example

--- web-crawler/Main.hs
+++ web-crawler/Main.hs
@@ -3,38 +3,53 @@ module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified System.IO as IO
-import Data.List (filter)
-
-import Data.List
-import Control.Monad.IO.Class (MonadIO)
-import Network.HTTP.Simple
+import Data.List (filter, isPrefixOf)
+-- import Control.Monad.IO.Class (MonadIO)
+import Network.HTTP.Simple (parseRequest, httpLBS, getResponseHeader, getResponseBody)
 
 type URL = String
 type Title = String
 type Contents = L8.ByteString
 type ResponseBody = L8.ByteString
 
-pageUrl = "http://link.springer.com/openurl?genre=book&isbn=978-0-306-48048-5"
-
 main :: IO ()
 main = do
   isbnDump <- IO.readFile "./ebook-isbns.txt"
-  responseBody <- download pageUrl
-  let pdfUrl = getPdfUrl responseBody
-  print(pdfUrl)
- -- >>= parsePage  >>= print
-  -- >>= download >>= parsePDF >>= writePDF
-  print ("DONE")
+  processIsbnList $ lines isbnDump
 
--- parseBody :: ResponseBody -> PDFUrl
-getPdfUrl body = "http://link.springer.com" ++ (takeWhile (/= '\"') $ drop 6 $ head $ filter ("href=\"/content" `isPrefixOf`) $ words $ L8.unpack body)
+processIsbnList [] = print (" --- DONE, ENJOY READING --- ")
+processIsbnList (x:xs) = do
+  print ("Processing isbn : " ++ x ++ " (" ++ show (length xs) ++ " remaining)")
+  pageReq <- parseRequest ("http://link.springer.com/openurl?genre=book&isbn=" ++ x)
+  print ("Getting download link...")
+  pageResponse <- download pageReq
+  print ("Received download link")
+  print ("Downloading book...")
+  downloadReq <- parseRequest (getPdfUrl $ getResponseBody pageResponse)
+  downloadRes <- download downloadReq
+  print ("Downloaded book")
+  let title = getBookTitle downloadRes
+  print ("Saving as " ++ title ++ ".pdf")
+  print ("---")
+  writePdf (title, getResponseBody downloadRes)
+  processIsbnList xs
 
-download :: MonadIO m => Request -> m (ResponseBody)
+getBookTitle res = takeWhile (/= '.')
+  $ drop 11
+  $ show (getResponseHeader "Content-Disposition" res)
+
+getPdfUrl body =  "http://link.springer.com" ++ (
+  takeWhile (/= '\"')
+  $ drop 6
+  $ head
+  $ filter ("href=\"/content" `isPrefixOf`)
+  $ words
+  $ L8.unpack body)
+
 download url = do
   response <- httpLBS url
-  return (getResponseBody response)
+  return response
 
-writePDF :: (Title, Contents) -> IO ()
-writePDF (title, contents) = do
+writePdf (title, contents) = do
   handle <- IO.openFile (title ++ ".pdf") IO.WriteMode
   L8.hPutStr handle contents
