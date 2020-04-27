b2451be158696f601e7e03bc2852cd67db0013f5
Pritesh Tupe <priteshtupe@gmail.com>
2020-04-27 21:59:41 +0530

    Added Readme, licence, pdf and type headers

--- web-crawler/Main.hs
+++ web-crawler/Main.hs
@@ -4,9 +4,10 @@ module Main where
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified System.IO as IO
 import Data.List (filter, isPrefixOf)
--- import Control.Monad.IO.Class (MonadIO)
-import Network.HTTP.Simple (parseRequest, httpLBS, getResponseHeader, getResponseBody)
+import Control.Monad.IO.Class (MonadIO)
+import Network.HTTP.Simple (Request, Response, parseRequest, httpLBS, getResponseHeader, getResponseBody)
 
+type ISBN = String
 type URL = String
 type Title = String
 type Contents = L8.ByteString
@@ -17,6 +18,7 @@ main = do
   isbnDump <- IO.readFile "./ebook-isbns.txt"
   processIsbnList $ lines isbnDump
 
+processIsbnList :: [ISBN] -> IO ()
 processIsbnList [] = print (" --- DONE, ENJOY READING --- ")
 processIsbnList (x:xs) = do
   print ("Processing isbn : " ++ x ++ " (" ++ show (length xs) ++ " remaining)")
@@ -24,8 +26,8 @@ processIsbnList (x:xs) = do
   print ("Getting download link...")
   pageResponse <- download pageReq
   print ("Received download link")
-  print ("Downloading book...")
   downloadReq <- parseRequest (getPdfUrl $ getResponseBody pageResponse)
+  print ("Downloading book...")
   downloadRes <- download downloadReq
   print ("Downloaded book")
   let title = getBookTitle downloadRes
@@ -34,10 +36,12 @@ processIsbnList (x:xs) = do
   writePdf (title, getResponseBody downloadRes)
   processIsbnList xs
 
+getBookTitle :: Response a -> Title
 getBookTitle res = takeWhile (/= '.')
   $ drop 11
   $ show (getResponseHeader "Content-Disposition" res)
 
+getPdfUrl :: ResponseBody -> URL
 getPdfUrl body =  "http://link.springer.com" ++ (
   takeWhile (/= '\"')
   $ drop 6
@@ -46,10 +50,12 @@ getPdfUrl body =  "http://link.springer.com" ++ (
   $ words
   $ L8.unpack body)
 
+download :: MonadIO m => Request -> m (Response L8.ByteString)
 download url = do
   response <- httpLBS url
   return response
 
+writePdf :: (Title, Contents) -> IO ()
 writePdf (title, contents) = do
   handle <- IO.openFile (title ++ ".pdf") IO.WriteMode
   L8.hPutStr handle contents
